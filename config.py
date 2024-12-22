48de34da47c1128bccaf8a1d0a9e4092a807ab4b
yehoste <yehoshuaste@gmail.com>
2024-12-22 13:53:54 +0200

    added json format

--- config.py
+++ config.py
@@ -24,9 +24,117 @@ hebrew_keywords = [
     "תרופות קבועות"
 ]
 
+json_format ={
+    "type": "object",
+    "required": ["מס משימה", "מס ניידת", "תאריך", "פרטי המטופל", "פרטי האירוע", "פירוט המקרה", "מדדים", "טיפולים", "טיפול תרופתי", "פינוי"],
+    "properties": {
+        "מס משימה": {"type": "string"},
+        "מס ניידת": {"type": "string"},
+        "תאריך": {"type": "string"},
+        "פרטי המטופל": {
+        "type": "object",
+        "required": ["סוג תעודה", "גיל", "מין"],
+        "properties": {
+            "סוג תעודה": {"type": "string"},
+            "גיל": {"type": "integer"},
+            "שם האב": {"type": "string"},
+            "מייל": {"type": "string"},
+            "מין": {"type": "string"},
+            "ת. לידה": {"type": "string"},
+            "קופת חולים": {"type": "string"},
+            "כתובת": {"type": "string"},
+            "שם מלא": {"type": "string"},
+            "טלפון": {"type": "string"},
+            "ישוב": {"type": "string"}
+        }
+        },
+        "פרטי האירוע": {
+        "type": "object",
+        "required": ["כתובת", "מקום האירוע", "עיר"],
+        "properties": {
+            "כתובת": {"type": "string"},
+            "מקום האירוע": {"type": "string"},
+            "עיר": {"type": "string"}
+        }
+        },
+        "פירוט המקרה": {
+        "type": "object",
+        "required": ["המקרה שנמצא", "תלונה עיקרית", "אנמנזה", "סטטוס המטופל"],
+        "properties": {
+            "המקרה שנמצא": {"type": "string"},
+            "תלונה עיקרית": {"type": "string"},
+            "אנמנזה": {"type": "string"},
+            "סטטוס המטופל": {"type": "string"},
+            "רקע רפואי": {"type": "string"},
+            "רגישויות": {"type": "string"},
+            "תרופות קבועות": {"type": "string"}
+        }
+        },
+        "מדדים": {
+        "type": "array",
+        "items": {
+            "type": "object",
+            "required": ["זמן בדיקה", "הכרה", "נשימה"],
+            "properties": {
+            "זמן בדיקה": { "type": "string" },
+                "הכרה": { "type": "string" },
+                "נשימה": { "type": "string" },
+                "קצב נשימה": { "type": "string" },
+                "דופק": { "type": "string" },
+                "דופק לדקה": { "type": "string" },
+                "מצב העור": { "type": "string" },
+                "סרגל כאב": { "type": "string" },
+                "האזנה": { "type": "string" },
+                "ריאה ימין": { "type": "string" },
+                "ריאה שמאל": { "type": "string" },
+                "ETCO2": { "type": "string" },
+                "קצב לב": { "type": "string" },
+                "אישונים": { "type": "string" },
+                "ציון גלזגו": { "type": "string" }       
+                }
+            }
+        }
+        },
+        "טיפולים": {
+        "type": "array",
+        "items": {
+            "type": "object",
+            "required": ["זמן", "טיפול שניתן"],
+            "properties": {
+            "זמן": {"type": "string"},
+            "טיפול שניתן": {"type": "array", "items": {"type": "string"}}
+            }
+        }
+        },
+        "טיפול תרופתי": {
+        "type": "array",
+        "items": {
+            "type": "object",
+            "required": ["זמן", "תרופה"],
+            "properties": {
+            "זמן": {"type": "string"},
+            "תרופה": {"type": "array", "items": {"type": "string"}}
+            }
+        }
+        },
+        "פינוי": {
+        "type": "object",
+        "required": ["אופן הפינוי", "יעד הפינוי", "שם בית החולים"],
+        "properties": {
+            "אופן הפינוי": {"type": "string"},
+            "יעד הפינוי": {"type": "string"},
+            "שם בית החולים": {"type": "string"},
+            "מחלקה": {"type": "string"},
+            "שם המקבל ביעד הפינוי": {"type": "string"}
+        }
+        }
+    }
+
 CHATGPT_REQUIREMENTS = f"""
 You are a text processor.I will provide you with a Hebrew text and a list of hebrew keywords.
 Your task is to extract the values corresponding to these keywords and return a valid JSON object.
 hebrew keywords:
-{hebrew_keywords}                                                                     
+{hebrew_keywords}
+fill this json object with the values you find in the user input text:
+{json_format}
 """
