63e64af8424a8c49a91a3b757900ba2fe2e18141
yehoste <yehoshuaste@gmail.com>
2024-12-23 15:56:40 +0200

    modifyed main to include gemini api

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 from backend.src import speech_to_text
 from backend.src import send_data_to_gpt
 from backend.src import compare_to_other_cases
+from backend.src import gemini_api
 import json
 import sys
 
@@ -17,7 +18,11 @@ def main():
         print("Speech-to-Text for Hebrew")
         audio = speech_to_text.record_audio()
         transcribed_audio = speech_to_text.transcribe_audio(audio)
-        response = send_data_to_gpt.chat_with_gpt(transcribed_audio)
+
+        response_gemini = gemini_api.get_gimini_response(transcribed_audio)    #this is for gemini
+
+        response = send_data_to_gpt.chat_with_gpt(transcribed_audio)    #this is for chatgpt
+
         json_response = json.loads(response)
         for key, value in json_response.items():                        #items is a method that returns a view object. The view object contains the key-value pairs of the dictionary, as tuples in a list.
             if key not in our_json:
